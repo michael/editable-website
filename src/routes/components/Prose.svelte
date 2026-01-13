9592740c4117a420dd4cec0f817fd56e6181ca59
Michael Aufreiter <michael@letsken.com>
2026-01-13 22:43:44 +0100

    Tweaks.

--- src/routes/components/Prose.svelte
+++ src/routes/components/Prose.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { getContext } from 'svelte';
 	import { Node, NodeArrayProperty } from 'svedit';
-	import { TW_PAGE_PADDING_X, TW_MOBILE_LEFT_INSET, TW_LIMITER } from '../tailwind_theme.js';
+	import { TW_PAGE_PADDING_X, TW_BLOCK_PADDING_Y, TW_MOBILE_LEFT_INSET, TW_LIMITER } from '../tailwind_theme.js';
 
 	const svedit = getContext('svedit');
 	let { path } = $props();
@@ -12,7 +12,7 @@
 <!-- Layout 1: Left-aligned -->
 {#snippet layout_1()}
 	<div class="{TW_LIMITER}">
-		<div class="xl:border-r xl:border-l py-8">
+		<div class="xl:border-r xl:border-l {TW_BLOCK_PADDING_Y}">
 			<div class="{TW_PAGE_PADDING_X} max-w-4xl">
 				<NodeArrayProperty path={[...path, 'content']} />
 			</div>
@@ -23,7 +23,7 @@
 <!-- Layout 2: Centered -->
 {#snippet layout_2()}
 	<div class="{TW_LIMITER}">
-		<div class="xl:border-r xl:border-l">
+		<div class="xl:border-r xl:border-l {TW_BLOCK_PADDING_Y}">
 			<div class="{TW_PAGE_PADDING_X} mx-auto max-w-4xl text-center">
 				<NodeArrayProperty path={[...path, 'content']} />
 			</div>
@@ -34,7 +34,7 @@
 <!-- Layout 3: Right-aligned -->
 {#snippet layout_3()}
 	<div class="{TW_LIMITER}">
-		<div class="xl:border-r xl:border-l grid grid-cols-3">
+		<div class="xl:border-r xl:border-l grid grid-cols-3 {TW_BLOCK_PADDING_Y}">
 			<!-- IMPORTANT: Keep in sync with TW_PAGE_PADDING_X -->
 			<div class="max-sm:pl-5 max-md:pl-7 pr-5 sm:pr-7 md:pr-10 lg:pr-14 col-span-3 md:col-span-2 md:col-start-2">
 				<div class="{TW_MOBILE_LEFT_INSET}">
--- src/routes/tailwind_theme.js
+++ src/routes/tailwind_theme.js
@@ -4,7 +4,9 @@
 export const TW_LIMITER = 'mx-auto max-w-8xl xl:px-8';
 
 export const TW_PAGE_PADDING_X = 'px-5 sm:px-7 md:px-10 lg:px-14';
+export const TW_BLOCK_PADDING_Y = 'py-12 md:py-14 lg:py-18';
+
 // When you want to sync vertical padding with the horizontal page padding
 export const TW_PAGE_PADDING = 'p-5 sm:p-7 md:p-10 lg:p-14';
 // Extra insert on mobile, to visualize "right-orientation" on mobile
-export const TW_MOBILE_LEFT_INSET = 'max-sm:pl-12 max-md:pl-16';
+export const TW_MOBILE_LEFT_INSET = 'max-sm:pl-16 max-md:pl-24';
