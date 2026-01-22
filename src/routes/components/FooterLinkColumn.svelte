077a500b611358750613d77972926e2b5a5d89d6
Michael Aufreiter <michael@letsken.com>
2026-01-22 22:25:44 +0100

    Cleanup

--- src/routes/components/FooterLinkColumn.svelte
+++ src/routes/components/FooterLinkColumn.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <Node {path}>
-	<div class="footer-link-column flex flex-col gap-2" style="container-type: inline-size; --layout-orientation: vertical;">
+	<div class="footer-link-column flex flex-col gap-2">
 		<AnnotatedTextProperty
 			class="font-bold"
 			path={[...path, 'label']}
