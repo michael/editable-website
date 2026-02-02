f5960c49c15fa5fc0bd82fcf98cdd6d2b4156b49
Michael Aufreiter <michael@letsken.com>
2026-02-02 19:29:28 +0100

    Spacing and colors

--- src/routes/components/Prose.svelte
+++ src/routes/components/Prose.svelte
@@ -7,14 +7,24 @@
 	let { path } = $props();
 	let node = $derived(svedit.session.get(path));
 	let layout = $derived(node.layout || 1);
+
+	const heading_spacing = `
+		[&>div:has(h1)~div>h1]:pt-8
+		[&>div:has(h2)~div>h2]:pt-6
+		[&>div:has(h3)~div>h3]:pt-4
+	`;
 </script>
+
+{#snippet content()}
+	<NodeArrayProperty class="space-y-8 {heading_spacing}" path={[...path, 'content']} />
+{/snippet}
 
 <!-- Layout 1: Left-aligned -->
 {#snippet layout_1()}
 	<div class="{TW_LIMITER}">
 		<div class="border-(--foreground-subtle) xl:border-r xl:border-l {TW_BLOCK_PADDING_Y}">
 			<div class="{TW_PAGE_PADDING_X} max-w-4xl">
-				<NodeArrayProperty class="space-y-4" path={[...path, 'content']} />
+				{@render content()}
 			</div>
 		</div>
 	</div>
@@ -25,7 +35,7 @@
 	<div class="{TW_LIMITER}">
 		<div class="border-(--foreground-subtle) xl:border-r xl:border-l {TW_BLOCK_PADDING_Y}">
 			<div class="{TW_PAGE_PADDING_X} mx-auto max-w-4xl text-center">
-				<NodeArrayProperty class="space-y-4" path={[...path, 'content']} />
+				{@render content()}
 			</div>
 		</div>
 	</div>
@@ -38,7 +48,7 @@
 			<!-- IMPORTANT: Keep in sync with TW_PAGE_PADDING_X -->
 			<div class="max-sm:pl-5 max-md:pl-7 pr-5 sm:pr-7 md:pr-10 lg:pr-14 col-span-3 md:col-span-2 md:col-start-2">
 				<div class="{TW_MOBILE_LEFT_INSET}">
-					<NodeArrayProperty class="space-y-4" path={[...path, 'content']} />
+					{@render content()}
 				</div>
 			</div>
 		</div>
