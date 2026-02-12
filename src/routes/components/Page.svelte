32a8d6611c84e39ed9cbda0085e0f614bc56b428
Michael Aufreiter <michael@letsken.com>
2026-02-12 19:00:19 +0100

    Inherit footer color from last body node

--- src/routes/components/Page.svelte
+++ src/routes/components/Page.svelte
@@ -10,6 +10,10 @@
   let first_body_node_id = $derived(node.body?.[0]);
   let first_body_node = $derived(first_body_node_id ? svedit.session.get([first_body_node_id]) : null);
   let nav_colorset_class = $derived(first_body_node?.colorset ? `ew-colorset-${first_body_node.colorset}` : '');
+
+  let last_body_node_id = $derived(node.body?.[node.body.length - 1]);
+  let last_body_node = $derived(last_body_node_id ? svedit.session.get([last_body_node_id]) : null);
+  let footer_colorset_class = $derived(last_body_node?.colorset ? `ew-colorset-${last_body_node.colorset}` : '');
 </script>
 
 <Node {path}>
@@ -21,7 +25,9 @@
     	<NodeArrayProperty class="body-node-array" path={[...path, 'body']} />
     </div>
 
-    <Footer path={[...path, 'footer']}/>
+    <div class="bg-(--background) text-(--foreground) {footer_colorset_class}">
+    	<Footer path={[...path, 'footer']}/>
+    </div>
   </div>
 </Node>
 
