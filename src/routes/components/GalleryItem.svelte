ecdb98ca62a952f787eea0b57480c6f18fce097d
Michael Aufreiter <michael@letsken.com>
2026-03-23 17:49:38 +0100

    Simplify GalleryItem

--- src/routes/components/GalleryItem.svelte
+++ src/routes/components/GalleryItem.svelte
@@ -1,18 +1,17 @@
 <script>
-	import { getContext } from 'svelte';
 	import { Node } from 'svedit';
 	import MediaProperty from './MediaProperty.svelte';
 	import { reveal } from '../reveal.js';
-
-	const svedit = getContext('svedit');
 	let { path } = $props();
 	let item_index = $derived(typeof path[path.length - 1] === 'number' ? path[path.length - 1] : 0);
 </script>
 
 <Node class="gallery-item" {path}>
-  <div use:reveal={{ delay: item_index * 150 }}>
-	<div class="overflow-hidden" style:border-radius="var(--image-border-radius)">
-		<MediaProperty class="image-property" inner_class="image-wrapper" path={[...path, 'media']} aspect_ratio="container" />
+	<div
+		class="image-wrapper overflow-hidden"
+		style:border-radius="var(--image-border-radius)"
+		use:reveal={{ delay: item_index * 150 }}
+	>
+		<MediaProperty class="image-property h-full" path={[...path, 'media']} aspect_ratio="container" />
 	</div>
-  </div>
 </Node>
