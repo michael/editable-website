b0d71548dab0f9453e4aba3fda0534aa2f91bbb2
Michael Aufreiter <michael@letsken.com>
2026-06-02 19:11:02 +0200

    Better spacing

--- src/routes/components/Accordion.svelte
+++ src/routes/components/Accordion.svelte
@@ -1,16 +1,12 @@
 <script>
 	import { Node, NodeArrayProperty } from 'svedit';
-	import { TW_PAGE_PADDING_X, TW_PAGE_PADDING } from '../tailwind_theme.js';
-
 	let { path } = $props();
 </script>
 
 <Node class="ew-accordion" {path}>
 	<div class="mx-auto max-w-4xl bg-(--background) text-(--foreground)">
-		<div class={TW_PAGE_PADDING}>
-			<div class={TW_PAGE_PADDING_X}>
-				<NodeArrayProperty class="flex flex-col" path={[...path, 'items']} />
-			</div>
+		<div class="px-5 sm:px-7 md:px-10 lg:px-14 py-10 sm:py-14 md:py-20 lg:py-28">
+			<NodeArrayProperty class="flex flex-col" path={[...path, 'items']} />
 		</div>
 	</div>
 </Node>
