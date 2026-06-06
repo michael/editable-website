86603faf9ff70ce3b23458f55eb13cef69eebc78
Michael Aufreiter <michael@letsken.com>
2026-06-06 18:28:45 +0200

    Introduce center-oriented layout for Prose

--- src/routes/components/Prose.svelte
+++ src/routes/components/Prose.svelte
@@ -21,10 +21,10 @@
 	<NodeArrayProperty class="flex flex-col gap-5 md:gap-8 {heading_spacing}" path={[...path, 'content']} />
 {/snippet}
 
-<!-- Layout 1: Left-aligned -->
+<!-- Layout 1: Left-oriented -->
 {#snippet layout_1()}
 	<div class="{TW_LIMITER}">
-		<div class="py-10 sm:py-14 md:py-16 lg:py-20">
+		<div class="py-10 sm:py-14 md:py-16 lg:py-28">
 			<div class="{TW_PAGE_PADDING_X} max-w-4xl">
 				{@render content()}
 			</div>
@@ -32,21 +32,21 @@
 	</div>
 {/snippet}
 
-<!-- Layout 2: Centered -->
+<!-- Layout 2: Center-oriented -->
 {#snippet layout_2()}
 	<div class="{TW_LIMITER}">
-		<div class="py-10 sm:py-14 md:py-16 lg:py-20">
-			<div class="{TW_PAGE_PADDING_X} mx-auto max-w-4xl text-center">
+		<div class="py-10 sm:py-14 md:py-16 lg:py-28">
+			<div class="{TW_PAGE_PADDING_X} mx-auto max-w-4xl">
 				{@render content()}
 			</div>
 		</div>
 	</div>
 {/snippet}
 
-<!-- Layout 3: Right-aligned -->
+<!-- Layout 3: Right-oriented -->
 {#snippet layout_3()}
 	<div class="{TW_LIMITER}">
-		<div class="grid grid-cols-3 py-10 sm:py-14 md:py-16 lg:py-20">
+		<div class="grid grid-cols-3 py-10 sm:py-14 md:py-16 lg:py-28">
 			<!-- IMPORTANT: Keep in sync with TW_PAGE_PADDING_X -->
 			<div class="max-sm:pl-5 max-md:pl-7 pr-5 sm:pr-7 md:pr-10 lg:pr-14 col-span-3 md:col-span-2 md:col-start-2">
 				<div class="{TW_MOBILE_LEFT_INSET} max-w-4xl">
@@ -57,7 +57,18 @@
 	</div>
 {/snippet}
 
+<!-- Layout 4: Centered -->
+{#snippet layout_4()}
+	<div class="{TW_LIMITER}">
+		<div class="py-10 sm:py-14 md:py-16 lg:py-28">
+			<div class="{TW_PAGE_PADDING_X} mx-auto max-w-4xl text-center">
+				{@render content()}
+			</div>
+		</div>
+	</div>
+{/snippet}
+
 <Node class="ew-prose lg:text-lg layout-{layout} bg-(--background) text-(--foreground) {colorset_class}" {path}>
-	{@const layouts = [layout_1, layout_2, layout_3]}
+	{@const layouts = [layout_1, layout_2, layout_3, layout_4]}
 	{@render layouts[layout - 1]()}
 </Node>
