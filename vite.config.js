cfe5040d98f441fa52da5f2916bef5fd8c79ae04
Michael Aufreiter <michael@letsken.com>
2026-06-07 09:21:28 +0200

    Upgrade Svelte+Kit and get rid of svelte.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,29 @@
+import adapter_auto from '@sveltejs/adapter-auto';
+import adapter_node from '@sveltejs/adapter-node';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+const adapter = process.env.VERCEL ? adapter_auto : adapter_node;
+
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
+	plugins: [
+		tailwindcss(),
+		sveltekit({
+			adapter: adapter(),
+			experimental: {
+				remoteFunctions: true
+			},
+			// alias: {
+			// 	'svedit': '../svedit/src/lib/index.js',
+			// }
+			compilerOptions: {
+				experimental: {
+					async: true
+				}
+			}
+		})
+	],
 	optimizeDeps: {
 		exclude: ['@jsquash/webp']
 	},
