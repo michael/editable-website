142392e67773c5ffaf962e63babaafe16f04e0e4
Michael Aufreiter <michael@letsken.com>
2026-06-10 15:27:06 +0200

    Use Label component in FooterLinkColumn

--- src/routes/components/FooterLinkColumn.svelte
+++ src/routes/components/FooterLinkColumn.svelte
@@ -1,19 +1,13 @@
 <script>
-	import { getContext } from 'svelte';
-	import { AnnotatedTextProperty, NodeArrayProperty, Node } from 'svedit';
-	const svedit = getContext('svedit');
+	import { NodeArrayProperty, Node } from 'svedit';
+	import Label from './Label.svelte';
 
 	let { path } = $props();
-	let node = $derived(svedit.session.get(path));
 </script>
 
 <Node {path}>
 	<div class="footer-link-column flex flex-col items-center lg:items-start gap-3 lg:gap-6">
-		<AnnotatedTextProperty
-			class="text-xs md:text-sm uppercase tracking-widest text-(--foreground) opacity-60"
-			path={[...path, 'label']}
-			placeholder="Column Label"
-		/>
+		<Label path={[...path, 'label']} placeholder="Column Label"/>
 		<NodeArrayProperty
 			class="footer-links w-full flex flex-col items-center lg:items-stretch"
 			path={[...path, 'footer_links']}
