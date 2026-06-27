8e1071ef38623b06d357b2fc799169a2e01a4c0e
Michael Aufreiter <michael@letsken.com>
2026-06-27 23:59:19 +0200

    Improve section marker alignment

--- src/routes/components/Section.svelte
+++ src/routes/components/Section.svelte
@@ -5,15 +5,14 @@
 	let { children, ...rest } = $props();
 
 	const bracket_class =
-		'pointer-events-none absolute left-5 right-5 z-1 h-4 select-none sm:left-7 sm:right-7';
-	const corner_class =
-		'absolute size-3.5 border-(--svedit-canvas-stroke)';
+		'pointer-events-none absolute left-1 right-1 z-1 h-2 select-none sm:left-2 sm:right-2 sm:h-4';
+	const corner_class = 'absolute size-2 border-(--svedit-canvas-stroke) sm:size-3.5';
 </script>
 
 <section {...rest}>
 	{#if svedit.editable}
 		<div class="relative">
-			<div class="{bracket_class} top-4" contenteditable="false" aria-hidden="true">
+			<div class="{bracket_class} top-1.5 sm:top-4" contenteditable="false" aria-hidden="true">
 				<div class="{corner_class} top-0 left-0 border-t border-l"></div>
 				<div class="{corner_class} top-0 right-0 border-t border-r"></div>
 			</div>
@@ -24,7 +23,7 @@
 	{/if}
 	{#if svedit.editable}
 		<div class="relative">
-			<div class="{bracket_class} bottom-4" contenteditable="false" aria-hidden="true">
+			<div class="{bracket_class} bottom-1.5 sm:bottom-4" contenteditable="false" aria-hidden="true">
 				<div class="{corner_class} bottom-0 left-0 border-b border-l"></div>
 				<div class="{corner_class} right-0 bottom-0 border-r border-b"></div>
 			</div>
